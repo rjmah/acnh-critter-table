75921e241b610e6784e0cacb488d2f04d1f53765
rjmah <ryanjamesmah@gmail.com>
2020-04-20 00:16:15 -0700

    Make time cell prettier

--- src/components/cells/time-cell.jsx
+++ src/components/cells/time-cell.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classNames from 'classnames';
 
-import { FULL_DAY_ARRAY } from 'Utility/constants';
+import {
+  FULL_DAY_ARRAY,
+  TIME_FORMAT_12,
+  TIME_FORMAT_24,
+} from 'Utility/constants';
+import { StateContext } from 'Reducer';
 
 function TimeCell({
   activeHours,
@@ -9,16 +14,52 @@ function TimeCell({
   currentHour,
   currentMinute,
 }) {
+  const state = useContext(StateContext);
+
   return (
     <div>
+      <div
+        className={classNames('hour_labels', {
+          'hour_labels--12': state.timeFormat === TIME_FORMAT_12,
+          'hour_labels--24': state.timeFormat === TIME_FORMAT_24,
+        })}
+      >
+        {state.timeFormat === TIME_FORMAT_12 ? (
+          <React.Fragment>
+            <div className="hour_label">
+              AM
+              <br />
+              12
+            </div>
+            <div className="hour_label">6</div>
+            <div className="hour_label">
+              PM
+              <br />
+              12
+            </div>
+            <div className="hour_label">6</div>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <div className="hour_label">0</div>
+            <div className="hour_label">6</div>
+            <div className="hour_label">12</div>
+            <div className="hour_label">18</div>
+          </React.Fragment>
+        )}
+      </div>
       <div className="hour_container">
         {FULL_DAY_ARRAY.map((hour, i) => (
-          <div
-            key={i}
-            className={classNames('hour_square', {
-              'hour_square--active': activeHours.has(i),
-            })}
-          />
+          <div key={i} className="hour_square">
+            {activeHours.has(i) && (
+              <div
+                className={classNames('hour_square__active', {
+                  'hour_square__active--start': !activeHours.has((i + 23) % 24),
+                  'hour_square__active--end': !activeHours.has((i + 1) % 24),
+                })}
+              />
+            )}
+          </div>
         ))}
         <div
           className="hour_container__current_time_marker"
@@ -26,6 +67,7 @@ function TimeCell({
             left: 4 * currentHour + Math.floor(currentMinute / 15),
           }}
         />
+        <div className="hour_container__end_marker" />
       </div>
       <div className="hour_text">
         {activeHoursText.map((text, i) => (
